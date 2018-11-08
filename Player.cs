d34ac3181724800cd64d70f5aa4d3d3f07e485dd
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-07 21:22:56 -0600

    First commit

---
+++ Player.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace RealisticBlood
+{
+    public class Player : ModPlayer
+    {
+        float redTimer = 0;
+
+        public override void OnHitByProjectile(Projectile proj, int damage, bool crit) {
+            redTimer = 1;
+        }
+
+        public override void OnHitByNPC(NPC npc, int damage, bool crit) {
+            redTimer = 1;
+        }
+
+        public override void ModifyDrawInfo(ref PlayerDrawInfo drawInfo) {
+            if (redTimer > 0) {
+                Color playerColor = drawInfo.bodyColor;
+
+                float bloodColor = redTimer;
+
+                float red = playerColor.R / 255f;
+                float green = playerColor.B / 255f;
+                float blue = playerColor.G / 255f;
+
+                drawInfo.bodyColor = new Color(Utils.Clamp(bloodColor, red, 1f), green, blue);
+                drawInfo.legColor = new Color(Utils.Clamp(bloodColor, red, 1f), green, blue);
+                drawInfo.faceColor = new Color(Utils.Clamp(bloodColor, red, 1f), green, blue);
+
+                player.immuneNoBlink = true;
+                redTimer -= 0.02f;
+            }
+        }
+
+        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright) {
+            if (redTimer > 0.8) {
+                Dust.NewDust(player.position, player.width, player.height, 183);
+            }
+        }
+    }
+}
---
+++ RealisticBlood.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace RealisticBlood
+{
+	class RealisticBlood : Mod
+	{
+		public RealisticBlood()
+		{
+		}
+	}
+}
